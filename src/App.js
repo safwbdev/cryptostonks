30488cd8f199b752cbfc431832d9ef99f2e2f982
Stefaan <safwebdev@gmail.com>
2021-09-26 09:46:34 +0800

    install & apply react router dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Switch>
+        <Route exact path="/">
+          Home
+        </Route>
+        <Route exact path="/exchanges">
+          Exchanges
+        </Route>
+        <Route exact path="/cryptocurrencies">
+          Cryptocurrencies
+        </Route>
+        <Route exact path="/crypto">
+          Coin details
+        </Route>
+        <Route exact path="/news">
+          News
+        </Route>
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
