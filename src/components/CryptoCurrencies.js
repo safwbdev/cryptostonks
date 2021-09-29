8b990fad2784158160491346b7ee271a018885ab
Stefaan <safwebdev@gmail.com>
2021-09-29 16:19:42 +0800

    styling fix

--- src/components/CryptoCurrencies.js
+++ src/components/CryptoCurrencies.js
@@ -7,7 +7,6 @@ import Slider from "react-slick";
 
 const CryptoCurrencies = ({ simplified }) => {
   const count = simplified ? 10 : 100;
-  const column = simplified ? 4 : 6;
   const { data: cryptoList, isFetching } = useGetCryptosQuery(count);
   const [cryptos, setCryptos] = useState([]);
   const [search, setSearch] = useState("");
@@ -74,13 +73,7 @@ const CryptoCurrencies = ({ simplified }) => {
         }
       >
         {cryptos?.map((currency) => (
-          <Col
-            xs={24}
-            sm={12}
-            lg={column}
-            className="crypto-card"
-            key={currency.id}
-          >
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
             <Link to={`/crypto/${currency.id}`}>
               <Card
                 title={`${currency.rank} | ${currency.name}`}
