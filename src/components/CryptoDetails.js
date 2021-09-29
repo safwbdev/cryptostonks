6167243dbe6ebb8025278e40ce9b2b48904c089a
Stefaan <safwebdev@gmail.com>
2021-09-29 08:35:10 +0800

    update global stats

--- src/components/CryptoDetails.js
+++ src/components/CryptoDetails.js
@@ -181,9 +181,7 @@ const CryptoDetails = () => {
             </Title>
             {cryptoDetails.links.map((link) => (
               <Row className="coin-link" key={link.name}>
-                <Title level={5} className="link-name">
-                  {link.type}
-                </Title>
+                <p className="link-name">{link.type}</p>
                 <a href={link.url} target="_blank" rel="noreferrer">
                   {link.name}
                 </a>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography, Row, Col, Statistic } from "antd";
+import { Typography, Row, Col, Statistic, Card } from "antd";
 import { CryptoCurrencies, News } from ".";
 import { Link } from "react-router-dom";
 import { useGetCryptosQuery } from "../services/cryptoApi";
@@ -10,6 +10,7 @@ const { Title } = Typography;
 const Home = () => {
   const { data, isFetching } = useGetCryptosQuery(10);
   const globalStats = data?.data?.stats;
+  const column = 8;
 
   if (isFetching) return "Loading";
   //   console.log(globalStats);
@@ -19,33 +20,46 @@ const Home = () => {
       <Title level={2} className="heading">
         Global Crypto Statistics
       </Title>
-      <Row>
-        <Col span={12}>
-          <Statistic title="Total CryptoCurrencies" value={globalStats.total} />
+      <Row gutter={[32, 32]} className="global-stat-row">
+        <Col xs={24} sm={12} lg={column}>
+          <Card className="global-stat-card">
+            <Statistic
+              title="Total CryptoCurrencies"
+              value={globalStats.total}
+            />
+          </Card>
         </Col>
-        <Col span={12}>
-          <Statistic
-            title="Total Exchange"
-            value={millify(globalStats.totalExchanges)}
-          />
+        <Col xs={24} sm={12} lg={column}>
+          <Card className="global-stat-card">
+            <Statistic
+              title="Total Exchange"
+              value={millify(globalStats.totalExchanges)}
+            />
+          </Card>
         </Col>
-        <Col span={12}>
-          <Statistic
-            title="Total Market Cap"
-            value={millify(globalStats.totalMarketCap)}
-          />
+        <Col xs={24} sm={12} lg={column}>
+          <Card className="global-stat-card">
+            <Statistic
+              title="Total Market Cap"
+              value={millify(globalStats.totalMarketCap)}
+            />
+          </Card>
         </Col>
-        <Col span={12}>
-          <Statistic
-            title="Total 24 Hour Volume"
-            value={millify(globalStats.total24hVolume)}
-          />
+        <Col xs={24} sm={12} lg={column}>
+          <Card className="global-stat-card">
+            <Statistic
+              title="Total 24 Hour Volume"
+              value={millify(globalStats.total24hVolume)}
+            />
+          </Card>
         </Col>
-        <Col span={12}>
-          <Statistic
-            title="Total Markets"
-            value={millify(globalStats.totalMarkets)}
-          />
+        <Col xs={24} sm={12} lg={column}>
+          <Card className="global-stat-card">
+            <Statistic
+              title="Total Markets"
+              value={millify(globalStats.totalMarkets)}
+            />
+          </Card>
         </Col>
       </Row>
       <div className="home-heading-container">
